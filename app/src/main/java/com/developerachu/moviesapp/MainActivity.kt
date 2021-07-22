d1d684fb93121d0c09293cfb2cadb53ee04febad
DeveloperAchu <achuunnikrishnan1@gmail.com>
2021-07-22 23:53:18 +0100

    popular shows page name changed, main activity code commented

--- app/src/main/java/com/developerachu/moviesapp/MainActivity.kt
+++ app/src/main/java/com/developerachu/moviesapp/MainActivity.kt
@@ -1,24 +1,33 @@
 package com.developerachu.moviesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 import com.developerachu.moviesapp.utils.AppConstants
 
+//This will be the main activity from which the app execution starts. It extends AppCompatActivity class
 class MainActivity : AppCompatActivity() {
+    //    On create function sets the activity's content view file
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        Get the button view by its id here and register an onclick listener
         val goButton: Button = findViewById(R.id.go_button)
         goButton.setOnClickListener {
-            navigateToNextScreen()
+            navigateToMoviesListScreen()
         }
 
     }
 
-    private fun navigateToNextScreen() {
-        println(AppConstants.TAG_NAME + "Navigating to top rated movies screen")
+    //    This function is called the go button is clicked. This function navigates the user to the
+    //    screen where popular tv shows are shown
+    private fun navigateToMoviesListScreen() {
+        Log.d(AppConstants.TAG_NAME, "Navigating to Movies list screen")
+        val intent = Intent(this, PopularTvShowsActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/developerachu/moviesapp/PopularTvShowsActivity.kt
@@ -0,0 +1,11 @@
+package com.developerachu.moviesapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PopularTvShowsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_popular_tv_shows)
+    }
+}
